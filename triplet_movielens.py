a66b09e98e251ceea1425ab403465dc89bc1a2ef
maciejkula <maciej.kula@gmail.com>
2016-12-18 19:47:11 -0800

    Update code to newest Keras. Move from dense to embedding layers.

--- triplet_movielens.py
+++ triplet_movielens.py
@@ -2,19 +2,14 @@
 Triplet loss network example for recommenders
 """
 
-
 from __future__ import print_function
 
 import numpy as np
 
-import theano
-
-import keras
 from keras import backend as K
-from keras.models import Sequential, Graph
-from keras.layers.core import Dense, Lambda
-from keras.optimizers import Adagrad, Adam
-
+from keras.models import Model
+from keras.layers import Embedding, Flatten, Input, merge
+from keras.optimizers import Adam
 
 import data
 import metrics
@@ -27,93 +22,47 @@ def identity_loss(y_true, y_pred):
 
 def bpr_triplet_loss(X):
 
-    user_latent, item_latent = X.values()
-    positive_item_latent, negative_item_latent = item_latent.values()
+    positive_item_latent, negative_item_latent, user_latent = X
 
     # BPR loss
-    loss = - K.sigmoid(K.sum(user_latent * positive_item_latent, axis=-1, keepdims=True)
-                       - K.sum(user_latent * negative_item_latent, axis=-1, keepdims=True))
+    loss = -K.sigmoid(
+        K.sum(user_latent * positive_item_latent, axis=-1, keepdims=True) -
+        K.sum(user_latent * negative_item_latent, axis=-1, keepdims=True))
 
     return loss
 
 
-def margin_triplet_loss(X):
+def build_model(num_users, num_items, latent_dim):
 
-    user_latent, item_latent = X.values()
-    positive_item_latent, negative_item_latent = item_latent.values()
+    positive_item_input = Input((1, ), name='positive_item_input')
+    negative_item_input = Input((1, ), name='negative_item_input')
 
-    # Hinge loss: max(0, user * negative_item_latent + 1 - user * positive_item_latent)
-    loss = K.maximum(1.0
-                     + K.sum(user_latent * negative_item_latent, axis=-1, keepdims=True)
-                     - K.sum(user_latent * positive_item_latent, axis=-1, keepdims=True),
-                     0.0)
+    # Shared embedding layer for positive and negative items
+    item_embedding_layer = Embedding(
+        num_items, latent_dim, name='item_embedding', input_length=1)
 
-    return loss
+    user_input = Input((1, ), name='user_input')
 
+    positive_item_embedding = Flatten()(item_embedding_layer(
+        positive_item_input))
+    negative_item_embedding = Flatten()(item_embedding_layer(
+        negative_item_input))
+    user_embedding = Flatten()(Embedding(
+        num_users, latent_dim, name='user_embedding', input_length=1)(
+            user_input))
 
-def get_item_subgraph(input_shape, latent_dim):
-    # Could take item metadata here, do convolutional layers etc.
+    loss = merge(
+        [positive_item_embedding, negative_item_embedding, user_embedding],
+        mode=bpr_triplet_loss,
+        name='loss',
+        output_shape=(1, ))
 
-    model = Sequential()
-    model.add(Dense(latent_dim, input_shape=input_shape))
-
-    return model
-
-
-def get_user_subgraph(input_shape, latent_dim):
-    # Could do all sorts of fun stuff here that takes
-    # user metadata in.
-
-    model = Sequential()
-    model.add(Dense(latent_dim, input_shape=input_shape))
-
-    return model
-
-
-def get_graph(num_users, num_items, latent_dim):
-
-    batch_input_shape = (1,)
-
-    model = Graph()
-
-    # Add inputs
-    model.add_input('user_input', input_shape=(num_users,), batch_input_shape=batch_input_shape)
-    model.add_input('positive_item_input', input_shape=(num_items,), batch_input_shape=batch_input_shape)
-    model.add_input('negative_item_input', input_shape=(num_items,), batch_input_shape=batch_input_shape)
-
-    # Add shared-weight item subgraph
-    model.add_shared_node(get_item_subgraph((num_items,), latent_dim),
-                          name='item_latent',
-                          inputs=['positive_item_input',
-                                  'negative_item_input'],
-                          merge_mode='join')
-    # Add user embedding
-    model.add_node(get_user_subgraph((num_users,), latent_dim),
-                   name='user_latent',
-                   input='user_input')
-
-    # Compute loss
-    model.add_node(Lambda(bpr_triplet_loss),
-                   name='triplet_loss',
-                   inputs=['user_latent', 'item_latent'],
-                   merge_mode='join')
-
-    # Add output
-    model.add_output(name='triplet_loss', input='triplet_loss')
-
-    # Compile using a dummy loss to fit within the Keras paradigm
-    model.compile(loss={'triplet_loss': identity_loss}, optimizer=Adam())#Adagrad(lr=0.1, epsilon=1e-06))
+    model = Model(
+        input=[positive_item_input, negative_item_input, user_input],
+        output=loss)
+    model.compile(loss=identity_loss, optimizer=Adam())
 
     return model
-
-
-def count_inversions(model, user_features, posititve_item_features, negative_item_features):
-
-    loss = model.predict({'user_input': user_features,
-                          'positive_item_input': posititve_item_features,
-                          'negative_item_input': negative_item_features})['triplet_loss']
-
-    return (loss > 0).mean()
 
 
 if __name__ == '__main__':
@@ -126,17 +75,11 @@ if __name__ == '__main__':
 
     # Prepare the test triplets
     test_uid, test_pid, test_nid = data.get_triplets(test)
-    test_user_features, test_positive_item_features, test_negative_item_features = data.get_dense_triplets(test_uid,
-                                                                                                           test_pid,
-                                                                                                           test_nid,
-                                                                                                           num_users,
-                                                                                                           num_items)
 
     # Sample triplets from the training data
     uid, pid, nid = data.get_triplets(train)
-    user_features, positive_item_features, negative_item_features = data.get_dense_triplets(uid, pid, nid, num_users, num_items)
 
-    model = get_graph(num_users, num_items, 256)
+    model = build_model(num_users, num_items, 10)
 
     # Print the model structure
     print(model.summary())
@@ -148,20 +91,17 @@ if __name__ == '__main__':
 
         print('Epoch %s' % epoch)
 
-        model.fit({'user_input': user_features,
-                   'positive_item_input': positive_item_features,
-                   'negative_item_input': negative_item_features, 'triplet_loss': np.ones(len(uid))},
-                  validation_data={'user_input': test_user_features,
-                                   'positive_item_input': test_positive_item_features,
-                                   'negative_item_input': test_negative_item_features, 'triplet_loss': np.ones(len(uid))},
+        X = {
+            'user_input': uid,
+            'positive_item_input': pid,
+            'negative_item_input': nid
+        }
+
+        model.fit(X,
+                  np.ones(len(uid)),
                   batch_size=512,
-                  nb_epoch=1, 
+                  nb_epoch=1,
                   verbose=2,
                   shuffle=True)
 
         print('AUC %s' % metrics.full_auc(model, test))
-        print('Inversions percentage %s' % count_inversions(model,
-                                                            test_user_features,
-                                                            test_positive_item_features,
-                                                            test_negative_item_features))
-
